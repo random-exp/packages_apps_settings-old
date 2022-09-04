bdbc47f1ce7ee7254e18611fbbe16692869f688a
maxwen <max.weninger@gmail.com>
2022-09-04 12:34:44 +0530

    Settings: Add BlazeHouse to dashboard
    Change-Id: Ie18a29e62a651b4b5c74510493dc291135f5361c

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -448,4 +448,7 @@ public class Settings extends SettingsActivity {
      * Activity for OneHandedSettings
      */
     public static class OneHandedSettingsActivity extends SettingsActivity { /* empty */ }
+
+    // BlazeHouse Additoon
+    public static class BlazeHouseActivity extends SettingsActivity {}
 }
